1e86e4adbc7a7931df5f6fdbde96065e43e34fae
KUMARASWAMY(surya) ROKKAM <rokkamkumara@gmail.com>
2025-07-03 22:59:14 +0530

    added AutoSuggestText project

--- src/Auto suggest text/AutoSuggestText.js
+++ src/Auto suggest text/AutoSuggestText.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 
 const AutoSuggestText = () =>
 {
-    const [userData,setUserData]=useState({})
+    const [userData,setUserData]=useState([])
     const [error,setError]= useState("")
     const [loading,setLoading]=useState(false);
+    const [searchItem,setSearchItem]=useState("")
+    const [suggestedText,setSuggestedText]=useState([])
+
 
     const fetchData = async (url) =>
     {
@@ -14,42 +17,86 @@ const AutoSuggestText = () =>
         const data= await response.json();
         if(data)
         {
-            console.log(data)
+           
             setLoading(false)
             setError("");
             setUserData(data.users.map((eachItem)=>eachItem.firstName))
             
         }
-        else
-        console.log("sdjfksj")
-        }
+    }
         catch(err)
         {
             setLoading(false)
-            setError("not found")
+            setError("not found error")
         }
         
 
     }
 
+    const searchFunction = () =>
+    {
+        const filtered = userData.filter((eachItem)=>
+        {
+           return eachItem.toLowerCase().includes(searchItem.toLowerCase())
+        })
+
+        setSuggestedText(filtered)
+
+    }
+
     useEffect(()=>
     {
+         
        fetchData("https://dummyjson.com/users")
+        
     },[])
+
+    useEffect(()=>
+    {
+       if(searchItem.length > 0)
+        {
+            searchFunction()
+        }
+        else
+        {setSuggestedText([])} 
+    },[userData,searchItem])
     return(
         <div>
             <h1><center> Project 14 (Auto Search Text)</center></h1>
             <div>
-                  {Object.entries(userData).map((eachUser,index)=>
-                {
-                    return(
                         < >
-                        <div key={index}>
-                         {eachUser}
+                         <div>
+                            <input type="text"
+                             value={searchItem}
+                             onChange={(e)=>setSearchItem(e.target.value)}
+                             />
                          </div>
+                         {error ? <h2>error......</h2> :
+                          <div>{loading ? <h2> loading....</h2> :
+                         <div>
+                            {
+                                suggestedText.length > 0  ? <div>
+                                    
+                                   {suggestedText.map((eachItem,index)=>
+                                {
+                                  
+                                    return(
+                                          <div key={index}>
+                                            <h3><center>{eachItem}</center></h3>
+                                          </div>
+                                    
+                                    )
+                                })}
+                            </div>: <h3><center>No Data Found
+                                </center></h3>
+                            }
+                         </div>
+                            }  
+                            
+                         
+                            </div>}
                         </>
-                    )
-                })}
+                   
             </div>
         </div>
     )
